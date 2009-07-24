a2e80c7e0ff1aa39db2199989230a817489ecab5
Jeffrey D. King <jeff.king@weathersource.com>
2009-07-24 00:23:49 +0000

    updated changelog for v1.3
    git-svn-id: http://plugins.svn.wordpress.org/wp-typography/trunk@138606 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- php-typography/php-parser/change_log_parser.php
+++ php-typography/php-parser/change_log_parser.php
@@ -1,12 +1,16 @@
-version 1.0 - July 15, 2009
+1.3 - July 23, 2009
+
+Uninitialized variables corrected throughout.
+
+1.0 - July 15, 2009
 
 Removed beta tag
 
-version 1.0 beta 7 - July 10, 2009
+1.0 beta 7 - July 10, 2009
 
 added "/" as a valid word character so we could capture "this/that" as a word for processing (similar to "mother-in-law")
 Corrected error where characters from the Latin 1 Supplement Block were not recognized as word characters
 
-version 1.0 beta 1
+1.0 beta 1
 
 initial release
