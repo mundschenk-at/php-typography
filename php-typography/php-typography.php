2b9c63e4006d0ce20e8dc559631a68bc8f6ba936
Jeffrey D. King <jeff.king@weathersource.com>
2009-09-08 16:12:11 +0000

    Corrected bug in smart quote and single character word handling where the "0" character may be improperly duplicated
    changed version to 1.14
    
    git-svn-id: http://plugins.svn.wordpress.org/wp-typography/trunk@153110 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- php-typography/php-typography.php
+++ php-typography/php-typography.php
@@ -2,7 +2,7 @@
 /*
 	Project: PHP Typography
 	Project URI: http://kingdesk.com/projects/php-tyography/
-	Version: 1.14 beta 1
+	Version: 1.14
 
 
 	Copyright 2009, KINGdesk, LLC. Licensed under the GNU General Public License 2.0. If you use, modify and/or redistribute this software, you must leave the KINGdesk, LLC copyright information, the request for a link to http://kingdesk.com, and the web design services contact information unchanged. If you redistribute this software, or any derivative, it must be released under the GNU General Public License 2.0. This program is distributed without warranty (implied or otherwise) of suitability for any particular purpose. See the GNU General Public License for full license terms <http://creativecommons.org/licenses/GPL/2.0/>.
@@ -732,10 +732,10 @@ class phpTypography {
 		$encodings = array("ASCII","UTF-8");
 		$e = mb_detect_encoding($parsedHTMLtoken["value"]."a", $encodings);// ."a" is a hack; see http://www.php.net/manual/en/function.mb-detect-encoding.php#81936
 		if(!isset($e) || $e == "") $e = "ASCII";
-		if($prevChr) {
+		if($prevChr != "") {
 			$parsedHTMLtoken["value"] = mb_substr($parsedHTMLtoken["value"], 1, mb_strlen($parsedHTMLtoken["value"], $e), $e);
 		}
-		if($nextChr) {
+		if($nextChr != "") {
 			$parsedHTMLtoken["value"] = mb_substr($parsedHTMLtoken["value"], 0, mb_strlen($parsedHTMLtoken["value"], $e)-1, $e);
 		}
 				
@@ -1134,10 +1134,10 @@ class phpTypography {
 		$encodings = array("ASCII","UTF-8");
 		$e = mb_detect_encoding($parsedHTMLtoken["value"]."a", $encodings);// ."a" is a hack; see http://www.php.net/manual/en/function.mb-detect-encoding.php#81936
 		if(!isset($e) || $e == "") $e = "ASCII";
-		if($prevChr) {
+		if($prevChr != "") {
 			$parsedHTMLtoken["value"] = mb_substr($parsedHTMLtoken["value"], 1, mb_strlen($parsedHTMLtoken["value"], $e), $e);
 		}
-		if($nextChr) {
+		if($nextChr != "") {
 			$parsedHTMLtoken["value"] = mb_substr($parsedHTMLtoken["value"], 0, mb_strlen($parsedHTMLtoken["value"], $e)-1, $e);
 		}
 
