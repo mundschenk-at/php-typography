a3c6f476d916bf1f9c95059df127596d9b8d7c3a
Der Mundschenk & Compagnie <github@mundschenk.at>
2015-12-25 10:02:10 +0100

    Relicensed as "GPL v2 or later" in accordance with Jeff. "Link clause" dropped.

--- php-typography/php-typography-functions.php
+++ php-typography/php-typography-functions.php
@@ -3,47 +3,32 @@
 /**
  *  This file is part of wp-Typography.
  *
- *  Copyright 2014-2015 Peter Putzer.
+ *	Copyright 2014-2015 Peter Putzer.
+ *	Copyright 2012-2013 Marie Hogebrandt.
+ *	Coypright 2009-2011 KINGdesk, LLC.
  *
  *	This program is free software; you can redistribute it and/or
- *	modify it under the terms of the GNU General Public License,
- *	version 2 as published by the Free Software Foundation.
+ *  modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
  *
- *	This program is distributed in the hope that it will be useful,
- *	but WITHOUT ANY WARRANTY; without even the implied warranty of
- *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *	GNU General Public License for more details.
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
  *
- *	You should have received a copy of the GNU General Public License
- *	along with this program; if not, write to the Free Software
- *	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
- *  MA 02110-1301, USA.
- *
- *  ***
- *
- *  Copyright 2009, KINGdesk, LLC. Licensed under the GNU General Public
- *  License 2.0. If you use, modify and/or redistribute this software,
- *  you must leave the KINGdesk, LLC copyright information, the request
- *  for a link to http://kingdesk.com, and the web design services
- *  contact information unchanged. If you redistribute this software, or
- *  any derivative, it must be released under the GNU General Public
- *  License 2.0.
- *
- *  This program is distributed without warranty (implied or otherwise) of
- *  suitability for any particular purpose. See the GNU General Public
- *  License for full license terms <http://creativecommons.org/licenses/GPL/2.0/>.
- *
- *  WE DON'T WANT YOUR MONEY: NO TIPS NECESSARY! If you enjoy this plugin,
- *  a link to http://kingdesk.com from your website would be appreciated.
- *  For web design services, please contact jeff@kingdesk.com.
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  *
  *  ***
  *
- *  @package wpTypography
+ *  @package wpTypography/PHPTypography
  *  @author Jeffrey D. King <jeff@kingdesk.com>
  *  @author Peter Putzer <github@mundschenk.at>
  *  @license http://www.gnu.org/licenses/gpl-2.0.html
  */
+
 
 namespace PHP_Typography;
 
