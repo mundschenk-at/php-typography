b995e37d2d07c6e3fbbb5dd8bc66afc482176809
Der Mundschenk & Compagnie <github@mundschenk.at>
2015-12-30 15:16:27 +0100

    Test: moved bootstrapping code its own file & fixed code coverage generation

---
+++ tests/bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Include debug helpers.
+ */
+require_once dirname( __DIR__ ) . '/php-typography/php-typography-debug.php';
+
+/**
+ * Autoloading.
+ */
+require_once dirname( __DIR__ ) . '/php-typography/php-typography-autoload.php';
