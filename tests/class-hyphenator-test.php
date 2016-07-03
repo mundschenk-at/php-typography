cbb82c0ba53f672b7f576c4e83e1804bd8458ef6
Der Mundschenk & Compagnie <github@mundschenk.at>
2016-07-03 10:15:15 +0200

    Tests: More working Hyphenator tests

--- tests/class-hyphenator-test.php
+++ tests/class-hyphenator-test.php
@@ -59,17 +59,45 @@ class Hyphenator_Test extends PHPUnit_Framework_TestCase
     }
 
     /**
+     * Helper function to generate a valid token list from strings.
+     *
+     * @param string $value
+     * @param string $type Optional. Default 'word'.
+     *
+     * @return array
+     */
+    protected function tokenize_sentence( $value ) {
+    	$words = explode( ' ', $value );
+    	$tokens = array();
+
+    	foreach ( $words as $word ) {
+    		$token[] = array(
+    			'type'  => 'word',
+    			'value' => $word,
+    		);
+    	}
+
+    	return $tokens;
+    }
+
+    /**
      *
      * @param string $expected_value
      * @param array $actual_tokens
      * @param string $message
      */
-    protected function assertTokenSame( $expected_value, $actual_tokens, $message = '' ) {
-    	foreach ( $actual_tokens as &$actual ) {
-    		$actual['value'] = $this->clean_html( $actual['value'] );
+    protected function assertTokensSame( $expected_value, $actual_tokens, $message = '' ) {
+    	foreach ( $actual_tokens as $index => $token ) {
+    		$actual_tokens[ $index ]['value'] = $this->clean_html( $actual_tokens[ $index ]['value'] );
     	}
 
-    	return $this->assertSame( $this->tokenize( $expected_value ) , $actual_tokens, $message );
+		if ( false !== strpos( $expected_value, ' ' ) ) {
+			$expected = $this->tokenize_sentence( $expected_value );
+		} else {
+			$expected = $this->tokenize( $expected_value );
+		}
+
+    	return $this->assertSame( $expected, $actual_tokens, $message );
     }
 
     /**
@@ -214,22 +242,20 @@ class Hyphenator_Test extends PHPUnit_Framework_TestCase
 
     public function provide_hyphenate_data() {
     	return array(
-    		array( 'A few words to hyphenate, like KINGdesk. Really, there should be more hyphenation here!', 'A few words to hy&shy;phen&shy;ate, like KING&shy;desk. Re&shy;ally, there should be more hy&shy;phen&shy;ation here!', 'en-US', true, true, true, false ),
-    		array( 'Sauerstofffeldflasche', 'Sau&shy;er&shy;stoff&shy;feld&shy;fla&shy;sche', 'de', true, true, true, false ),
-    		array( 'Sauerstoff-Feldflasche', 'Sau&shy;er&shy;stoff-Feld&shy;fla&shy;sche', 'de', true, true, true, true ),
-    		array( 'Sauerstoff-Feldflasche', 'Sauerstoff-Feldflasche', 'de', true, true, true, false ),
+    		array( 'A few words to hyphenate, like KINGdesk. Really, there should be more hyphenation here!', 'A few words to hy|phen|ate, like KING|desk. Re|ally, there should be more hy|phen|ation here!', 'en-US', true ),
+    		array( 'Sauerstofffeldflasche', 'Sau|er|stoff|feld|fla|sche', 'de', true ),
+    		array( 'Sauerstoff-Feldflasche', 'Sau|er|stoff-Feld|fla|sche', 'de', true ),
+    		array( 'Sauerstoff-Feldflasche', 'Sauerstoff-Feldflasche', 'de', true ),
     	);
     }
 
     /**
      * @covers ::hyphenate
-     * @covers ::hyphenate
-     * @covers ::hyphenate_compounds
      * @covers ::hyphenation_pattern_injection
      *
      * @dataProvider provide_hyphenate_data
      */
-    public function test_hyphenate( $html, $result, $lang, $hyphenate_headings, $hyphenate_all_caps, $hyphenate_title_case, $hyphenate_compunds )
+    public function test_hyphenate( $html, $result, $lang, $hyphenate_title_case )
     {
     	$h = $this->h;
     	$h->set_language( $lang );
@@ -238,33 +264,14 @@ class Hyphenator_Test extends PHPUnit_Framework_TestCase
     	$h->set_min_after(2);
     	$h->set_custom_exceptions( array( 'KING-desk' ) );
 
-    	/*	$this->assertSame( "This is a paragraph with no embedded hyphenation hints and no hyphen-related CSS applied. Corporate gibberish follows. Think visionary. If you generate proactively, you may have to e-enable interactively. We apply the proverb \"Grass doesn't grow on a racetrack\" not only to our re-purposing but our power to matrix. If all of this comes off as dumbfounding to you, that's because it is! Our feature set is unparalleled in the industry, but our reality-based systems and simple use is usually considered a remarkable achievement. The power to brand robustly leads to the aptitude to embrace seamlessly. What do we streamline? Anything and everything, regardless of reconditeness",
-    	 $this->clean_html( $this->object->hyphenate("This is a paragraph with no embedded hyphenation hints and no hyphen-related CSS applied. Corporate gibberish follows. Think visionary. If you generate proactively, you may have to e-enable interactively. We apply the proverb \"Grass doesn't grow on a racetrack\" not only to our re-purposing but our power to matrix. If all of this comes off as dumbfounding to you, that's because it is! Our feature set is unparalleled in the industry, but our reality-based systems and simple use is usually considered a remarkable achievement. The power to brand robustly leads to the aptitude to embrace seamlessly. What do we streamline? Anything and everything, regardless of reconditeness") ) );
-    	*/
-
-    	$this->assertSame( $result, clean_html( $h->hyphenate( $html ) ) );
-    }
-
-    /**
-     * @covers ::hyphenate
-     */
-    public function test_hyphenate_headings_disabled()
-    {
-    	$this->h->set_language( 'en-US' );
-    	$this->h->set_min_length(2);
-    	$this->h->set_min_before(2);
-    	$this->h->set_min_after(2);
-    	$this->h->set_custom_exceptions( array( 'KING-desk' ) );
-
-    	$html = '<h2>A few words to hyphenate, like KINGdesk. Really, there should be no hyphenation here!</h2>';
-    	$this->assertSame( $html, $this->clean_html( $this->h->hyphenate( $html ) ) );
+    	$this->assertSame( $this->tokenize_sentence( $result ), $h->hyphenate( $this->tokenize_sentence( $html ), '|', $hyphenate_title_case ) );
     }
 
     /**
      * @covers ::hyphenate
      * @covers ::hyphenation_pattern_injection
      */
-    public function test_hyphenate_2()
+    public function test_hyphenate_wrong_encoding()
     {
     	$this->h->set_language( 'de' );
     	$this->h->set_min_length(2);
@@ -272,12 +279,12 @@ class Hyphenator_Test extends PHPUnit_Framework_TestCase
     	$this->h->set_min_after(2);
 
     	$tokens = $this->tokenize( mb_convert_encoding( 'Änderungsmeldung', 'ISO-8859-2' ) );
-    	$hyphenated  = $this->h->hyphenate( $tokens );
-	   	$this->assertEquals( $hyphenated, $tokens );
+    	$hyphenated  = $this->h->hyphenate( $tokens, '|', true );
+	   	$this->assertSame( $hyphenated, $tokens, 'Wrong encoding, value should be unchanged' );
 
 	   	$tokens = $this->tokenize( 'Änderungsmeldung' );
-	   	$hyphenated  = $this->h->hyphenate( $tokens );
-	   	$this->assertNotEquals( $hyphenated, $tokens );
+	   	$hyphenated  = $this->h->hyphenate( $tokens, '|', true );
+	   	$this->assertNotSame( $hyphenated, $tokens, 'Correct encoding, string should have been hyphenated' );
     }
 
     /**
@@ -291,7 +298,7 @@ class Hyphenator_Test extends PHPUnit_Framework_TestCase
     	$this->h->set_min_after(2);
 
     	$tokens = $this->tokenize( 'Änderungsmeldung' );
-    	$hyphenated  = $this->h->hyphenate( $tokens );
+    	$hyphenated  = $this->h->hyphenate( $tokens, '|', false );
     	$this->assertEquals( $tokens, $hyphenated);
     }
 
@@ -302,10 +309,8 @@ class Hyphenator_Test extends PHPUnit_Framework_TestCase
     {
     	$this->h->set_language( 'de' );
     	$this->h->set_min_length(2);
-    	$this->h->set_min_before(2);
+    	$this->h->set_min_before(0);
     	$this->h->set_min_after(2);
-
-    	$this->h->settings['hyphenMinBefore'] = 0; // invalid value
 
     	$tokens = $this->tokenize( 'Änderungsmeldung' );
     	$hyphenated  = $this->h->hyphenate( $tokens );
@@ -322,8 +327,10 @@ class Hyphenator_Test extends PHPUnit_Framework_TestCase
     	$this->h->set_min_before(2);
     	$this->h->set_min_after(2);
 
-    	$this->assertSame( 'A few words to hy&shy;phen&shy;ate, like KINGdesk. Re&shy;ally, there should be more hy&shy;phen&shy;ation here!',
-    					   $this->clean_html( $this->h->hyphenate( 'A few words to hyphenate, like KINGdesk. Really, there should be more hyphenation here!' ) ) );
+    	$this->assertTokensSame(
+    		'A few words to hy|phen|ate, like KINGdesk. Re|ally, there should be more hy|phen|ation here!',
+    		$this->h->hyphenate( $this->tokenize_sentence( 'A few words to hyphenate, like KINGdesk. Really, there should be more hyphenation here!' ), '|', true )
+    	);
     }
 
     /**
@@ -338,7 +345,7 @@ class Hyphenator_Test extends PHPUnit_Framework_TestCase
 		$this->h->settings['hyphenationPatternExceptions'] = array();
 		unset( $this->h->settings['hyphenationExceptions'] );
 
-    	$this->assertSame( 'A few words to hy&shy;phen&shy;ate, like KINGdesk. Re&shy;ally, there should be more hy&shy;phen&shy;ation here!',
+    	$this->assertSame( 'A few words to hy|phen|ate, like KINGdesk. Re|ally, there should be more hy|phen|ation here!',
     					   $this->clean_html( $this->h->hyphenate( 'A few words to hyphenate, like KINGdesk. Really, there should be more hyphenation here!' ) ) );
     }
 }
