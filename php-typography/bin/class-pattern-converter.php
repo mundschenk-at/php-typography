632be48a979f84ab04f867bdd47f14abf6c3723c
Der Mundschenk & Compagnie <github@mundschenk.at>
2017-02-26 12:13:52 +0100

    Proper namespacing for patterns2json

--- php-typography/bin/class-pattern-converter.php
+++ php-typography/bin/class-pattern-converter.php
@@ -25,9 +25,10 @@
  *  @license http://www.gnu.org/licenses/gpl-2.0.html
  */
 
-namespace PHP_Typography;
+namespace PHP_Typography\Bin;
 
-define( 'WP_TYPOGRAPHY_DEBUG', true );
+use function \PHP_Typography\mb_str_split;
+use function \PHP_Typography\uchr;
 
 require_once( dirname( __DIR__ ) . '/php-typography-functions.php' );
 
@@ -299,48 +300,3 @@ class Pattern_Converter {
 		$this->write_results( $patterns, $exceptions, $comments );
 	}
 }
-
-$shortopts = 'l:f:hv';
-$longopts = array( 'lang:', 'file:', 'help', 'version' );
-
-$options = getopt( $shortopts, $longopts );
-
-// Print version.
-if ( isset( $options['v'] ) || isset( $options['version'] ) ) {
-	echo "wp-Typography hyhpenation pattern converter 2.0-beta\n\n";
-	die( 0 );
-}
-
-// Print help.
-if ( isset( $options['h'] ) || isset( $options['help'] ) ) {
-	echo "Usage: convert_pattern [arguments]\n";
-	echo "pattern2json -l <language> -f <filename>\t\tconvert <filename>\n";
-	echo "pattern2json --lang <language> --file <filename>\tconvert <filename>\n";
-	echo "pattern2json -v|--version\t\t\t\tprint version\n";
-	echo "pattern2json -h|--help\t\t\t\tprint help\n";
-	die( 0 );
-}
-
-// Read necessary options.
-if ( isset( $options['f'] ) ) {
-	$filename = $options['f'];
-} elseif ( isset( $options['file'] ) ) {
-	$filename = $options['file'];
-}
-if ( empty( $filename ) ) {
-	echo "Error: no filename\n";
-	die( -1 );
-}
-
-if ( isset( $options['l'] ) ) {
-	$language = $options['l'];
-} elseif ( isset( $options['lang'] ) ) {
-	$language = $options['lang'];
-}
-if ( empty( $language ) ) {
-	echo "Error: no language\n";
-	die( -2 );
-}
-
-$converter = new Pattern_Converter( $filename, $language );
-$converter->convert();
