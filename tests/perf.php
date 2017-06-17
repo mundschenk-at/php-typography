29ff54b4d3f1a23a086c520db9cdcc35e5a9d391
Der Mundschenk & Compagnie <github@mundschenk.at>
2017-06-17 18:26:23 +0200

    Tests: performance tests coding standards

--- tests/perf.php
+++ tests/perf.php
@@ -1,9 +1,10 @@
 <?php
-
 /**
  * Performance test suite for wp-Typography. Licensed under the terms of the GNU General Public License 2.0.
  *
- * (c) 2015 Peter Putzer
+ * (c) 2015, 2017 Peter Putzer
+ *
+ * @package WP_Typography\Tests
  */
 
 error_reporting( E_ALL & E_STRICT );
@@ -11,12 +12,12 @@ error_reporting( E_ALL & E_STRICT );
 require_once realpath( __DIR__ . '/../php-typography/php-typography-autoload.php' );
 require_once realpath( __DIR__ . '/../vendor/hyphenator-php/src/NikoNyrh/Hyphenator/Hyphenator.php' );
 
-// don't break without translation function
+// Don't break without translation function.
 if ( ! function_exists( '__' ) ) {
-	function &__( $string, $domain = null ) { return $string; }
+	function &__( $string, $domain = null ) { return $string; } // @codingStandardsIgnoreLine
 }
 
-$testHTML = <<<'EOD'
+$test_html = <<<'EOD'
 To deal with one page that has fixed width for text, the practical move would be to add a couple of SOFT HYPHEN characters (U+00AD), using the entity reference &shy; if you find it more comfortable than entering the (invisible) character itself. You can rather quickly find out which words need to be hyphenated to produce a good result.
 
 In a more complex case (several pages, flexible width), this includes foobar use a preprocessor, or server-side code, or client-side code that adds soft hyphens. The client-side approach is simplest and can be applied independently of server-side technologies and authoring tools. Beware that automatic hyphenation may go wrong and needs some help: the language(s) of the text need to be indicated in markup (or otherwise, depending on the library used).
@@ -54,38 +55,23 @@ $php_typo->set_min_after_hyphenation();
 
 $iterations = 100;
 
-$startTime = microtime( true );
+// Variant A.
+$start_time = microtime( true );
 for ( $i = 0; $i < $iterations; ++$i ) {
-	$php_typo->set_hyphenation_exceptions( ['foo-bar', 'pre-pro-ces-sor', 'fo-ll-o-w-i-ng'] );
-	$php_typo->process( $testHTML, false );
+	$php_typo->set_hyphenation_exceptions( [ 'foo-bar', 'pre-pro-ces-sor', 'fo-ll-o-w-i-ng' ] );
+	$php_typo->process( $test_html, false );
 }
-$endTime = microtime( true );
-echo "$i iterations took " . ( $endTime - $startTime ) . " seconds.\n";
+$end_time = microtime( true );
+echo "$i iterations took " . ( $end_time - $start_time ) . " seconds.\n"; // @codingStandardsIgnoreLine
 
-$startTime = microtime(true);
-
-//$state = $php_typo->save_state();
-//$foo = new \PHP_Typography\PHP_Typography( false, 'lazy' );
-//$foo->load_state( $state );
-// $syllable = new Syllable( 'en-US' );
-// $syllable->getCache()->setPath( __DIR__ . '/../vendor/phpSyllable' . '/cache' );
-// $syllable->getSource()->setPath( __DIR__ . '/../vendor/phpSyllable' . '/languages' );
-// $syllable->setHyphen(new Syllable_Hyphen_Soft);
-
-// for ( $i = 0; $i < 10; ++$i ) {
-// 	$syllable->hyphenateText($testHTML);
-// //	$foo->process( $testHTML, false );
-// }
-// $endTime = microtime( true );
-// echo "$i iteratations w/ phpSyllable took " . ( $endTime - $startTime ) . " seconds.\n";
-
-$startTime = microtime(true);
+// Variant B.
+$start_time = microtime( true );
 $hyphenator = new \NikoNyrh\Hyphenator\Hyphenator();
 $html = new \Masterminds\HTML5();
 for ( $i = 0; $i < $iterations; ++$i ) {
-	$html->loadHTML( $testHTML );
+	$html->loadHTML( $test_html );
 	$html->saveHTML();
-	$hyphenator->hyphenate($testHTML);
+	$hyphenator->hyphenate( $test_html );
 }
-$endTime = microtime( true );
-echo "$i iteratations w/ hyphenator-php took " . ( $endTime - $startTime ) . " seconds.\n";
+$end_time = microtime( true );
+echo "$i iteratations w/ hyphenator-php took " . ( $end_time - $start_time ) . " seconds.\n"; // @codingStandardsIgnoreLine
