5825dab0f84a1c42634095be5e00e8e73a8ea340
Der Mundschenk & Compagnie <github@mundschenk.at>
2017-07-19 22:08:49 +0200

    Tests: coverage for dash and quote styles

---
+++ tests/settings/class-dash-style-test.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ *  This file is part of wp-Typography.
+ *
+ *  Copyright 2017 Peter Putzer.
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or ( at your option ) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ *  @package wpTypography/Tests
+ *  @license http://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+namespace PHP_Typography\Tests\Settings;
+
+use \PHP_Typography\Tests\PHP_Typography_Testcase;
+
+use \PHP_Typography\Settings\Dashes;
+use \PHP_Typography\Settings\Dash_Style;
+use \PHP_Typography\U;
+
+/**
+ * Dash_Style unit test.
+ *
+ * @coversDefaultClass \PHP_Typography\Settings\Dash_Style
+ * @usesDefaultClass \PHP_Typography\Settings\Dash_Style
+ *
+ * @uses PHP_Typography\Settings\Simple_Dashes
+ */
+class Dash_Style_Test extends PHP_Typography_Testcase {
+
+
+	/**
+	 * Provide test data for testing get_styled_dashes.
+	 */
+	public function provide_get_styled_dashes_data() {
+		return [
+			[ Dash_Style::TRADITIONAL_US, [ U::EM_DASH, U::THIN_SPACE, U::EN_DASH, U::THIN_SPACE ] ],
+			[ Dash_Style::INTERNATIONAL, [ U::EN_DASH, ' ', U::EN_DASH, U::HAIR_SPACE ] ],
+			[ 'foo', null ],
+			[ 123, null ],
+		];
+	}
+
+	/**
+	 * Test get_styled_dashes.
+	 *
+	 * @covers ::get_styled_dashes
+	 *
+	 * @dataProvider provide_get_styled_dashes_data
+	 *
+	 * @param  mixed      $style  Style index.
+	 * @param  array|null $result Result array (or null).
+	 */
+	public function test_get_styled_dashes( $style, array $result = null ) {
+		$s = $this->createMock( \PHP_Typography\Settings::class );
+
+		$dashes = Dash_Style::get_styled_dashes( $style, $s );
+
+		if ( is_array( $result ) ) {
+			$this->assertInstanceOf( Dashes::class, $dashes );
+			$this->assertSame( $result[0], $dashes->parenthetical_dash() );
+			$this->assertSame( $result[1], $dashes->parenthetical_space() );
+			$this->assertSame( $result[2], $dashes->interval_dash() );
+			$this->assertSame( $result[3], $dashes->interval_space() );
+		} else {
+			$this->assertNull( $dashes, 'get_styled_dashes should return null for invalid indices.' );
+		}
+	}
+}
---
+++ tests/settings/class-simple-dashes-test.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ *  This file is part of wp-Typography.
+ *
+ *  Copyright 2017 Peter Putzer.
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or ( at your option ) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ *  @package wpTypography/Tests
+ *  @license http://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+namespace PHP_Typography\Tests\Settings;
+
+use \PHP_Typography\Tests\PHP_Typography_Testcase;
+
+use \PHP_Typography\Settings\Quotes;
+use \PHP_Typography\Settings\Simple_Dashes;
+use \PHP_Typography\U;
+
+/**
+ * Simple_Dashes unit test.
+ *
+ * @coversDefaultClass \PHP_Typography\Settings\Simple_Dashes
+ * @usesDefaultClass \PHP_Typography\Settings\Simple_Dashes
+ *
+ * @uses PHP_Typography\Settings\Simple_Dashes
+ */
+class Simple_Dashes_Test extends PHP_Typography_Testcase {
+
+	/**
+	 * Provide data for testing the Simple_Dashes class.
+	 *
+	 * @return array
+	 */
+	public function provide_simple_dashes_data() {
+		return [
+			[ 'a', 'b', 'c', 'd' ],
+			[ 'foo', 'bar', 'bay', 'baz' ],
+		];
+	}
+
+	/**
+	 * Test the class.
+	 *
+	 * @covers ::__construct
+	 * @covers ::parenthetical_dash
+	 * @covers ::parenthetical_space
+	 * @covers ::interval_dash
+	 * @covers ::interval_space
+	 *
+	 * @dataProvider provide_simple_dashes_data
+	 *
+	 * @param string $pdash  Required.
+	 * @param string $pspace Required.
+	 * @param string $idash  Required.
+	 * @param string $ispace Required.
+	 */
+	public function test_simple_sashes( $pdash, $pspace, $idash, $ispace ) {
+		$dashes = new Simple_Dashes( $pdash, $pspace, $idash, $ispace );
+
+		$this->assertSame( $pdash, $dashes->parenthetical_dash() );
+		$this->assertSame( $pspace, $dashes->parenthetical_space() );
+		$this->assertSame( $idash, $dashes->interval_dash() );
+		$this->assertSame( $ispace, $dashes->interval_space() );
+	}
+}
