6620595184ef6237c87399a88cf7cc9c319508c9
Der Mundschenk & Compagnie <github@mundschenk.at>
2017-08-05 12:37:50 +0200

    Re-add some meta files

---
+++ .phan/config.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * This configuration will be read and overlaid on top of the
+ * default configuration. Command line arguments will be applied
+ * after this file is read.
+ */
+return [
+
+	// A list of directories that should be parsed for class and
+	// method information. After excluding the directories
+	// defined in exclude_analysis_directory_list, the remaining
+	// files will be statically analyzed for errors.
+	//
+	// Thus, both first-party and third-party code being used by
+	// your application should be included in this list.
+	'directory_list' => [
+		'php-typography',
+		'vendor',
+	],
+
+	// A directory list that defines files that will be excluded
+	// from static analysis, but whose class and method
+	// information should be included.
+	//
+	// Generally, you'll want to include the directories for
+	// third-party code (such as "vendor/") in this list.
+	//
+	// n.b.: If you'd like to parse but not analyze 3rd
+	//       party code, directories containing that code
+	//       should be added to both the `directory_list`
+	//       and `exclude_analysis_directory_list` arrays.
+	'exclude_analysis_directory_list' => [
+		'vendor/',
+	],
+
+	// Add any issue types (such as 'PhanUndeclaredMethod')
+	// to this black-list to inhibit them from being reported.
+	'suppress_issue_types' => [
+		'PhanUndeclaredMethod',
+		'PhanUndeclaredFunction',
+		'PhanUndeclaredConstant',
+	],
+
+	// If true, missing properties will be created when
+	// they are first seen. If false, we'll report an
+	// error message if there is an attempt to write
+	// to a class property that wasn't explicitly
+	// defined.
+	'allow_missing_properties' => true,
+];
