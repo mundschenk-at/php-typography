9bb75f228a854988e38b9a9d566bf148581ecbc9
Der Mundschenk & Compagnie <github@mundschenk.at>
2017-09-10 01:11:32 +0200

    Fix GPL boilerplate formatting

--- tests/settings/class-quote-style-test.php
+++ tests/settings/class-quote-style-test.php
@@ -4,9 +4,9 @@
  *
  *  Copyright 2017 Peter Putzer.
  *
- *  This program is free software; you can redistribute it and/or
- *  modify it under the terms of the GNU General Public License
- *  as published by the Free Software Foundation; either version 2
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
  *  of the License, or ( at your option ) any later version.
  *
  *  This program is distributed in the hope that it will be useful,
@@ -14,9 +14,9 @@
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
  *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  *
  *  @package mundschenk-at/php-typography/tests
  *  @license http://www.gnu.org/licenses/gpl-2.0.html
