e711dc1e726fcab98ab83d105759efc89c07a199
Der Mundschenk & Compagnie <github@mundschenk.at>
2017-12-23 11:16:28 +0100

    Reduce if nesting

--- src/fixes/node-fixes/class-style-initial-quotes-fix.php
+++ src/fixes/node-fixes/class-style-initial-quotes-fix.php
@@ -82,25 +82,27 @@ class Style_Initial_Quotes_Fix extends Classes_Dependent_Fix {
 			return;
 		}
 
-		if ( '' === DOM::get_prev_chr( $textnode ) ) {
-			// We have the first text in a block level element.
-			$node_data       = $textnode->data;
-			$f               = Strings::functions( $node_data );
-			$first_character = $f['substr']( $node_data, 0, 1 );
+		if ( '' !== DOM::get_prev_chr( $textnode ) ) {
+			// This is not the first textnode, abort.
+			return;
+		}
 
-			if ( self::is_single_quote( $first_character ) ) {
-				$span_class = $this->single_quote_class;
-			} elseif ( self::is_double_quote( $first_character ) ) {
-				$span_class = $this->double_quote_class;
-			}
+		$node_data       = $textnode->data;
+		$f               = Strings::functions( $node_data );
+		$first_character = $f['substr']( $node_data, 0, 1 );
 
-			if ( ! empty( $span_class ) ) {
-				// Assume page title is <h2>.
-				$block_level_parent = $is_title ? 'h2' : DOM::get_block_parent_name( $textnode );
+		if ( self::is_single_quote( $first_character ) ) {
+			$span_class = $this->single_quote_class;
+		} elseif ( self::is_double_quote( $first_character ) ) {
+			$span_class = $this->double_quote_class;
+		}
 
-				if ( ! empty( $block_level_parent ) && isset( $settings['initialQuoteTags'][ $block_level_parent ] ) ) {
-					$textnode->data = '<span class="' . $span_class . '">' . $first_character . '</span>' . $f['substr']( $node_data, 1, $f['strlen']( $node_data ) );
-				}
+		if ( ! empty( $span_class ) ) {
+			// Assume page title is <h2>.
+			$block_level_parent = $is_title ? 'h2' : DOM::get_block_parent_name( $textnode );
+
+			if ( ! empty( $block_level_parent ) && isset( $settings['initialQuoteTags'][ $block_level_parent ] ) ) {
+				$textnode->data = '<span class="' . $span_class . '">' . $first_character . '</span>' . $f['substr']( $node_data, 1, $f['strlen']( $node_data ) );
 			}
 		}
 	}
