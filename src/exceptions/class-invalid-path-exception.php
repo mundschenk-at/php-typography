f2c7c6352c126bf8e3d7a1dc6f9dcff7fad12721
mundschenk_at <mundschenk-at@users.noreply.github.com>
2024-03-15 20:22:00 +0100

    Throw exception instead of relying on the native PHP warning

---
+++ src/exceptions/class-invalid-path-exception.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ *  This file is part of PHP-Typography.
+ *
+ *  Copyright 2024 Peter Putzer.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ *  ***
+ *
+ *  @package mundschenk-at/php-typography
+ *  @license http://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+namespace PHP_Typography\Exceptions;
+
+/**
+ * An invalid path was given.
+ *
+ * @author Peter Putzer <github@mundschenk.at>
+ *
+ * @since 6.8.0
+ */
+class Invalid_Path_Exception extends \RuntimeException {
+}
